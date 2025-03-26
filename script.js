74953c38519e9b701451760aa6401d275843d757
Leandro <silvaleandro69094@gmail.com>
2025-03-26 11:14:54 -0300

    calculadora

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const campo1 = document.querySelector("#campo1");
 const campo2 = document.querySelector("#campo2");
 const seletor = document.querySelector("#operacao");
-const botao = document.querySelector("#igual");
 let resposta = document.querySelector("#resposta");
 
-botao.addEventListener("click",calcular);
+seletor.addEventListener("change",calcular);
+campo1.addEventListener("keyup",calcular);
+campo2.addEventListener("keyup",calcular);
 
-function calcular() {
+function calcular(){
+
+    if(campo1.value!='' && campo2.value!=''){
+        
+    
     const valor1 = parseInt(campo1.value);
     const valor2 = parseInt(campo2.value);
     const operacao = seletor.value;
@@ -18,6 +23,7 @@ function calcular() {
         resposta.innerHTML = valor1 * valor2;
     if(operacao==="dividir")
         resposta.innerHTML = valor1 / valor2;
+    
+       }
+}    
 
-
-}
